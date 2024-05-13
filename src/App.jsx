f96eadff7fd9630b618101d6303122c9accc162e
kizito3 <chigokizzy@gmail.com>
2024-05-13 18:15:32 +0100

    created pages and routes

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import ForgotPassword from './pages/ForgotPassword'
+import Offers from './pages/Offers'
+
+function App() {
+
+  return (
+    <>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/profile' element={<Profile />}/>
+            <Route path='/sign-in' element={<Signin />}/>
+            <Route path='/sign-up' element={<Signup />}/>
+            <Route path='/forgot-password' element={<ForgotPassword />}/>
+            <Route path='/offers' element={<Offers />}/>
+          </Routes>
+        </Router>
+    </>
+  )
+}
+
+export default App
